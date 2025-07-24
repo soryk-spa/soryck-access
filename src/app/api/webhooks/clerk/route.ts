f08e7ee9f7496f1657fa7a6ba34ebe763912d174
Francisco Munoz <franciscomuniozs@gmail.com>
2025-07-24 15:04:21 -0400

    Fix#001 - Fixed typo error in Clerk webhook for prisma client

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -2,7 +2,7 @@ import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
 import { prisma } from '@/lib/prisma'
-import { UserRole } from '@/generated/prisma/client'
+import { UserRole } from '@prisma/client'
 
 export async function POST(req: Request) {
   const headerPayload = await headers();
